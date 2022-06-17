431609915598721b174039df94d7b47d21b1eb1c
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-17 21:24:31 +0530

    [Harini Test Case-1.2 and 1.3]: Adding State Census invalid File Data

--- IndianStates_Codes/StateCodeTesting/UnitTest1.cs
+++ IndianStates_Codes/StateCodeTesting/UnitTest1.cs
@@ -29,6 +29,55 @@ namespace IndianStatesAndCode_Testing
                 Assert.AreEqual("Invalid File", ex.Message);
             }
         }
+        //1.3
+        [Test]
+        public void GivenInvalidFileType_ShouldThrowInvalidFileTypeException()
+        {
+            try
+            {
+                int expected = 29;
+                string filePath = @"D:\.net\Indian-States---Census-Analyser\IndianStates_Codes\IndianStates_Codes\StateCensusData\StateCensusData .txt";
+                IndianStates_Codes.StatesAnalyser getMethod = new IndianStates_Codes.StatesAnalyser();
+                int actual = getMethod.DataAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid File Type", ex.Message);
+            }
+        }
+        /*[Test]
+        public void GivenInvalidDelimiterFile_ShouldThrowInvalidDelimiterFileTypeException()
+        {
+            try
+            {
+                int expected = 29;
+                string filePath = @"D:\.net\Indian-States---Census-Analyser\IndianStates_Codes\IndianStates_Codes\Invalid Delimiter StateCensusData.csv";
+                IndianStates_Codes.StatesAnalyser getMethod = new IndianStates_Codes.StatesAnalyser();
+                int actual = getMethod.DataAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Incorrect Delimiter", ex.Message);
+            }
+        }
+        [Test]
+        public void GivenIncorrectHeaderFile_ShouldThrowIncorrectHeaderFileTypeException()
+        {
+            try
+            {
+                int expected = 29;
+                string filePath = @"D:\.net\Indian-States---Census-Analyser\IndianStates_Codes\IndianStates_Codes\Invalid Delimiter StateCensusData.csv";
+                IndianStates_Codes.StatesAnalyser getMethod = new IndianStates_Codes.StatesAnalyser();
+                int actual = getMethod.DataAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Incorrect Header", ex.Message);
+            }
+        }*/
 
     }
 }
