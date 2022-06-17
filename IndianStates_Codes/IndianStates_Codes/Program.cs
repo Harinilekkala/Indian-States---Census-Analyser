d9bef301584bc86684a562e8f26f37699dd1bcf9
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-17 17:32:27 +0530

    [Harini]:Loading Census Data from CSV file

---
+++ IndianStates_Codes/IndianStates_Codes/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace IndianStates_Codes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filePath = @"D:\.net\Indian-States---Census-Analyser\IndianStates_Codes\IndianStates_Codes\StateCodeData\StateCensusData.csv";
+            IndianStates_Codes.StatesAnalyser  sc = new IndianStates_Codes.StatesAnalyser();
+
+            int obj = sc.DataAnalyser(filePath);
+        }
+    }
+}
