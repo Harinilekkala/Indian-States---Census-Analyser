6fac8c630808181aea5789a1f344e988d6bd2064
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-17 21:13:18 +0530

    [Harini Test Case-1.2]: Adding State Census invalid File Data

--- IndianStates_Codes/StateCodeTesting/UnitTest1.cs
+++ IndianStates_Codes/StateCodeTesting/UnitTest1.cs
@@ -12,6 +12,23 @@ namespace IndianStatesAndCode_Testing
             int actual = getMethod.DataAnalyser(filePath);
             Assert.AreEqual(expected, actual);
         }
-        
+        //1.2
+        [Test]
+        public void GivenInvalidFile_ShouldThrowInvalidFileException()
+        {
+            try
+            {
+                int expected = 29;
+                string filePath = @"D:\.net\Indian-States---Census-Analyser\IndianStates_Codes\IndianStates_Codes\StateCensusData\StateCensus.csv";
+                IndianStates_Codes.StatesAnalyser getMethod = new IndianStates_Codes.StatesAnalyser();
+                int actual = getMethod.DataAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid File", ex.Message);
+            }
+        }
+
     }
 }
